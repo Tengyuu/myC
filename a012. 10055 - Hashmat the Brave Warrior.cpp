fb3580b72eb15b3ad197e3cb89aaf5c70bf2ff53
Tengyuu <153273922+Tengyuu@users.noreply.github.com>
2025-03-05 00:26:41 +0800

    Create a012. 10055 - Hashmat the Brave Warrior.cpp
    有符號 long long int 的範圍是：-2^63 到 2^63 - 1。
    無符號 unsigned long long int 的範圍是：0 到 2^64 - 1。

---
+++ a012. 10055 - Hashmat the Brave Warrior.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <cmath>
+#include <bitset>
+using namespace std;
+int main()
+{
+	long long int a, b;
+	while (cin >> a >> b) {
+		cout << abs(a - b) <<"\n";
+	}
+}
