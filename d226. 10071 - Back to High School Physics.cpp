1bd29c52479ea46ee51a1f88c2f9e59684efb98c
Tengyuu <153273922+Tengyuu@users.noreply.github.com>
2025-03-06 14:42:55 +0800

    Create d226. 10071 - Back to High School Physics.cpp
    v-t圖下面積＝位移

---
+++ d226. 10071 - Back to High School Physics.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<map>
+
+using namespace std;
+
+
+int main(){
+    int v,t;
+    while(cin>>v>>t){
+        cout<<2*v*t<<"\n";
+    }
+}
