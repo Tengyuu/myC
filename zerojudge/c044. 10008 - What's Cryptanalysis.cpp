1e744e909e72027317bf790be0d881eaf381f20b
Tengyuu <153273922+Tengyuu@users.noreply.github.com>
2025-03-21 03:00:24 +0800

    Update c044. 10008 - What's Cryptanalysis.cpp

--- zerojudge/c044. 10008 - What's Cryptanalysis.cpp
+++ zerojudge/c044. 10008 - What's Cryptanalysis.cpp
@@ -1,43 +1,37 @@
-
 #include <iostream>
-#include <vector>
 #include <string>
+#include <map>
+#include <vector>
 #include <algorithm>
-#include <unordered_map>
 using namespace std;
 
-bool cmp(const pair<char,int>& a,const pair<char,int>& b){
-    if(a.second!=b.second){
-        return a.second>b.second;
-    }
-    else{
-        return a.first<b.first;
-    }
-
+bool cmp(const pair<char,int>& a,const pair<char,int>& b) {
+	return a.second > b.second;
+	if (a.second == b.second) {
+		return a.first < b.first;
+	}
 }
-
 int main()
 {
-    int n;cin>>n;
-    cin.ignore();
-    string s;
-    string ss = "";
-    unordered_map<char,int> freq;
-    for(int i=0;i<n;i++){
-        getline(cin,s);
-        ss+=s;
-    }
-    
-    for(char c:ss){
-        if(isalpha(c)){
-            c = toupper(c);
-            freq[c]++;
-        }
-    }
-    vector<pair<char,int>> a(freq.begin(),freq.end()); 
-    sort(a.begin(),a.end(),cmp);
-
-    for(auto i:a){
-        cout<<i.first<<" "<<i.second<<endl;
-    }
+	int n;
+	cin >> n;
+	cin.ignore();
+	map<char, int> map;
+	while (n--) {
+		string s;
+		getline(cin, s);
+		for (char c : s) {
+			if (isalpha(c)) {
+				c = toupper(c);
+				map[c]++;
+			}
+		}
+	}
+	vector<pair<char, int>> vec(map.begin(), map.end());
+		
+	sort(vec.begin(), vec.end(),cmp);
+	for (auto i : vec) {
+		cout << i.first << " " << i.second << "\n";
+	}
 }
+
