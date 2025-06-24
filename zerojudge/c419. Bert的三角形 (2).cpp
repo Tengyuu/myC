1d6822e1c8efa5dabd542dcb47f49e188ab0fd97
Tengyuu <153273922+Tengyuu@users.noreply.github.com>
2025-06-24 18:42:04 +0800

    Create c419. Bert的三角形 (2).cpp

---
+++ zerojudge/c419. Bert的三角形 (2).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+    for (int b = 1; b < a + 1; b++) {
+        cout << string(a-b, '_');
+        cout << string(b, '*')<<"\n";
+
+    }
+
+}
